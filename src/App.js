5494ae2232bbaa88fa5b09bab396264e640b10af
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-07-18 23:55:16 -0500

    app.js , variables

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
 
 function App() {
+    const name="mina";
+   const naver = {
+       name : "naver",
+       url : "https://naver.com"
+   }
   return (
     <div className="App">
       <h1 style={{
         color:"#f0f",
         backgroundColor:"green",
       }}>
-        Welcome!
+        Welcome! hello {name},
+        <p>{2+3+4}</p>
       </h1>
+        <a href={naver.url}>{naver.name}</a>
     </div>
   );
 }
