a254720edd2c723a22683e6113934013d100ba54
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-07-19 14:59:04 -0500

    component exercise

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import './App.css';
 
 function App() {
-    const name="mina";
-   const naver = {
-       name : "naver",
-       url : "https://naver.com"
-   }
+   //  const name="mina";
+   // const naver = {
+   //     name : "naver",
+   //     url : "https://naver.com"
+   // }
   return (
     <div className="App">
-      <h1 style={{
-        color:"#f0f",
-        backgroundColor:"green",
-      }}>
-        Welcome! hello {name},
-        <p>{2+3+4}</p>
-      </h1>
-        <a href={naver.url}>{naver.name}</a>
+      {/*<h1 style={{*/}
+      {/*  color:"#f0f",*/}
+      {/*  backgroundColor:"green",*/}
+      {/*}}>*/}
+      {/*  Welcome! hello {name},*/}
+      {/*  <p>{2+3+4}</p>*/}
+      {/*</h1>*/}
+      {/*  <a href={naver.url} target="_blank">{naver.name}</a>*/}
     </div>
   );
 }
---
+++ src/component/Hello.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const Hello = function(){
+    <p>hello</p>
+};
+
+export default Hello;
+
